90b628251d1e1cda1c1f95272eee9e30d5922809
Panagiotis Karagiannis <pkarsy@gmail.com>
2020-01-02 02:33:57 +0200

    Updaded examples

--- examples/BluePill_SPI2/BluePill_SPI2.ino
+++ examples/BluePill_SPI2/BluePill_SPI2.ino
@@ -1,8 +1,8 @@
 /*
     CC1101_RF library demo
     This is an example of a RF module on the SECOND SPI bus of a
-    bluePill / blackpill module. The GDO0 pin is selected to be near the other pins
-    This sketch can communicate with all other examples.
+    bluePill / blackpill module. For simplicity the GDo0 pin is not connected
+    to the MCU. This sketch can communicate with all other examples.
     The examples are on the public domain
 */
 
@@ -17,14 +17,13 @@
 //    CSK           PB13(SCK2)
 //    MISO          PB14(MISO2)
 //    MOSI          PA15(MOSI2)
-//    GDO0          PA8 // is near the other pins
 //    GND           GND
 //    VCC           3.3V
 
 
-SPIClass spi2(2); // for STM32duino the second spibus
+SPIClass spi2(2); // for STM32duino the second SPI bus
 
-//CC1101 pins CSN, MISO, SPIbus
+//CC1101 pins  CSN, MISO, SPIbus
 CC1101 radio( PB12, PB14, spi2   );
 
 // Uncomment to use the Serial1 port instead of the USB
@@ -33,7 +32,7 @@ CC1101 radio( PB12, PB14, spi2   );
 void setup() {
     Serial.begin(9600);
     Serial.println("BluePill_SPI2 begin");
-    SPI.begin(); // mandatory. CC1101_RF does not start SPI automatically
+    spi2.begin(); // mandatory. CC1101_RF does not start SPI automatically
     radio.begin(433.2e6); // Freq=433.2Mhz
     //
     // LED setup. It is importand as we can use the module without serial terminal
@@ -50,15 +49,14 @@ uint32_t pingTimer=0;
 uint32_t receiveTime;
 
 void loop() {
-    // Turn on the LED for 100ms without loop block. The Buildin LED on bluepill is ON when LOW
-    digitalWrite(LED_BUILTIN, millis()-receiveTime>100);
+    // Turn on the LED for 200ms without loop block. The Buildin LED on bluepill is ON when LOW
+    digitalWrite(LED_BUILTIN, millis()-receiveTime>200);
     // or external LED. The "<" is because this LED is ON when HIGH
-    // digitalWrite(PB9, millis()-receiveTime<100);
+    // digitalWrite(PB9, millis()-receiveTime<200);
 
 
     if ((millis()-pingTimer>5000)) { // ping every 5sec
         Serial.println("Sending ping");
-        // change the string to know who is sending
         if (radio.sendPacket("Ping from BluePill_SPI2")) {
             Serial.println("Ping sent");
         } else {
@@ -69,22 +67,17 @@ void loop() {
         pingTimer = millis();
     }
 
-    // Receive part. GDO0 is connected with PA8
-    if (digitalRead(PA8)) {
-        byte packet[64];
-        byte pkt_size = radio.getPacket(packet);
-        if (pkt_size>0 && radio.crcok()) { // We have a valid packet with some data
-            Serial.print("Got packet \"");
-            Serial.write(packet, pkt_size);
-            Serial.print("\" len=");
-            Serial.print(pkt_size);
-            Serial.print(" Signal="); // for field tests to check the signal strength
-            Serial.print(radio.getRSSIdbm());
-            Serial.print(" LQI="); // for field tests to check the signal quality
-            Serial.println(radio.getLQI());
-        } else {
-            Serial.println("No/Invalid packet");
-        }
+    byte packet[64];
+    byte pkt_size = radio.getPacket(packet);
+    if (pkt_size>0 && radio.crcok()) { // We have a valid packet with some data
+        Serial.print("Got packet \"");
+        Serial.write(packet, pkt_size);
+        Serial.print("\" len=");
+        Serial.print(pkt_size);
+        Serial.print(" Signal="); // for field tests to check the signal strength
+        Serial.print(radio.getRSSIdbm());
+        Serial.print(" LQI="); // for field tests to check the signal quality
+        Serial.println(radio.getLQI());
         receiveTime=millis();
     }
 }
